5a96af1fac48682c2ac8bc8f8d50f1510d398e5d
Pedro Matos <pedrom070903@gmail.com>
2026-04-27 23:37:08 -0300

    Exception Handler

--- course/src/main/java/springweb/course/service/UserService.java
+++ course/src/main/java/springweb/course/service/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import springweb.course.entities.User;
 import springweb.course.repositories.UserRepository;
+import springweb.course.service.exceptions.ResourceNotFoundException;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,7 +23,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = userRepository.findById(id);
-        return  obj.orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+        return  obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
---
+++ course/src/main/java/springweb/course/service/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package springweb.course.service.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(Object id){
+        super("Resource not found. Id " + id);
+    }
+}
